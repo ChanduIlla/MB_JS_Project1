056a158fe79aae1ad8577956560b46cc302a5785
Chandra Sekhar <illa.sekhar.19.1@mountblue.tech>
2022-03-12 09:32:00 +0530

    Fixed Bugs in Problem1

--- Test_Questions/tsetProblem1.js
+++ Test_Questions/tsetProblem1.js
@@ -1,4 +1,5 @@
 const findCar = require('../problem1') ;
 const inventory = require('../inventory');
-const result = findCar(inventory);
-console.log(`Car 33 is a ${result.car_year} ${result.car_make} ${result.car_model}`)
+let searchId = 33
+const result = findCar(inventory,searchId)
+console.log(result)
--- problem1.js
+++ problem1.js
@@ -1,10 +1,14 @@
-function findCar(inventory) {
-    let result ;
-    for (let i = 0 ; i < inventory.length ; i++) {
-        if ( inventory[i].id === 33) {
-            result = inventory[i];
-        }
+function findCar(inventory,searchId ) {
+    if ((typeof(searchId) === 'undefined') || (inventory.length === 0)) {
+        return []
     }
-    return result ;
+    if (inventory.length > 0) {
+        for (let i = 0 ; i < inventory.length ; i++) {
+            if ( inventory[i].id === searchId) {
+                let carObject = inventory[i];
+                return `Car ${carObject.id} is a ${carObject.car_year} ${carObject.car_make} ${carObject.car_model}`
+            }
+        }
+    } 
 }
 module.exports = findCar ;
